d6747dca9876a7b9fd01bb994d4a605c39a24606
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-10-02 20:46:09 +0200

    -

--- AD/Exercicios/U0_Repaso/TarefasAvaliables/AcademiaMas/src/modelo/Alumno.java
+++ AD/Exercicios/U0_Repaso/TarefasAvaliables/AcademiaMas/src/modelo/Alumno.java
@@ -1,10 +1,26 @@
 
 package modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author XoanOuteiro
  */
 public class Alumno {
+    
+    //Instance atributes
+    private String nombre;
+    private String localidad;
+    private ArrayList<Modulo> modulos;
+    
+    
+    //Build methods
+    
+    
+    //Getters & Setters
+    
+    
+    //Utility methods
     
 }
--- AD/Exercicios/U0_Repaso/TarefasAvaliables/AcademiaMas/src/modelo/Modulo.java
+++ AD/Exercicios/U0_Repaso/TarefasAvaliables/AcademiaMas/src/modelo/Modulo.java
@@ -7,4 +7,34 @@ package modelo;
  */
 public class Modulo {
     
+    //Instance atributes
+    private String nombre;
+    private int horas;
+    private int unidades;
+    
+    //Getters & Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+    
 }
