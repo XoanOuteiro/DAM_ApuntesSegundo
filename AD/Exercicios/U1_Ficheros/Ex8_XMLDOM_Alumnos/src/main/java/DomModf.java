2e0c1df29abdd711af476db9a4cd5e61bcc7bf25
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-10-20 12:02:48 +0200

    AD

---
+++ AD/Exercicios/U1_Ficheros/Ex8_XMLDOM_Alumnos/src/main/java/DomModf.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author XoanOuteiro
+ */
+public class DomModf {
+
+    
+    /*
+    private void removeWhitespaces(Element element) {
+        NodeList children = element.getChildNodes();
+        for (int i = children.getLength() - 1; i >= 0; i--) {
+            Node child = children.item(i);
+            if (child instanceof Text
+                    && ((Text) child).getData().trim().isEmpty()) {
+                element.removeChild(child);
+            } else if (child instanceof Element) {
+                removeWhitespaces((Element) child);
+            }
+        }
+    }
+    */
+
+}
---
+++ AD/Exercicios/U1_Ficheros/Ex8_XMLDOM_Alumnos/src/main/java/Ex8_XMLDOM_Alumnos.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author XoanOuteiro
+ */
+public class Ex8_XMLDOM_Alumnos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
