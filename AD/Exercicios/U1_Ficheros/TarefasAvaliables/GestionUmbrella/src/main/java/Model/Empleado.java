e38da6c55c3dda57960e9f08ea8e007ee1e3b4d6
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-10-27 12:43:13 +0200

    AD

--- AD/Exercicios/U1_Ficheros/TarefasAvaliables/GestionUmbrella/src/main/java/Model/Empleado.java
+++ AD/Exercicios/U1_Ficheros/TarefasAvaliables/GestionUmbrella/src/main/java/Model/Empleado.java
@@ -6,5 +6,19 @@ package Model;
  * @author XoanOuteiro
  */
 public class Empleado {
+ 
+    //Instance data
     
+    private String nif;
+    private String nombre;
+    private String apellidos;
+    private float salario;
+    
+    //Builder methods
+    
+    
+    //Instance methods
+    
+    
+    //Getters and Setters
 }
