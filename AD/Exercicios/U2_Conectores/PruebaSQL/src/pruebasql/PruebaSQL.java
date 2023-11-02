f1ed7426567e0041bfa9413158f73dd5e1a4cdfc
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-11-02 11:17:54 +0100

    SQLEando

---
+++ AD/Exercicios/U2_Conectores/PruebaSQL/src/pruebasql/PruebaSQL.java
@@ -0,0 +1,17 @@
+
+package pruebasql;
+
+/**
+ *
+ * @author XoanOuteiro
+ */
+public class PruebaSQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
