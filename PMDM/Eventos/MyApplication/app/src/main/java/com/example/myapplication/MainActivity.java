cf577b7375834cdba5960d149f61cc3709087f52
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-11-06 15:13:08 +0100

    PMDM

--- PMDM/Eventos/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ PMDM/Eventos/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,42 +13,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Create", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnCrete");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnStop");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnPause");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(this, "Restart", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "Destroy", Toast.LENGTH_SHORT).show();
+        Log.i("Ciclo","OnDestroy");
     }
 
 
