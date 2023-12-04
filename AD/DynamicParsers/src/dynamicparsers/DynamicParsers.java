b3fb1ceb990d1b486b1de82df12dec2b7595ac92
XoanOuteiro <xoanouteirojorge@gmail.com>
2023-12-04 09:08:56 +0100

    Updated validators

--- AD/DynamicParsers/src/dynamicparsers/DynamicParsers.java
+++ AD/DynamicParsers/src/dynamicparsers/DynamicParsers.java
@@ -1,6 +1,10 @@
 
 package dynamicparsers;
 
+import Validators.Validator;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * Main class to conduct tests on different parsers
@@ -13,7 +17,25 @@ public class DynamicParsers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Validator v = new Validator();
+        System.out.println(v.checkNif("00000000T"));
+        System.out.println(v.checkNif("000D00003"));
+        System.out.println(v.checkNif("000000002"));
+        
+        Scanner reads = new Scanner(System.in);
+        String in = reads.nextLine();
+        try{
+            
+            float f = v.parseDataFloat(in);
+            System.out.println(f);
+            
+        }catch(InputMismatchException e){
+            
+            System.out.println(e.getLocalizedMessage());
+            
+        }
+        
     }
     
 }
