e5bebfe3dadf6b322685f4ce23288dce4cbfcc50
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-02-05 12:12:18 +0100

    AD

---
+++ AD/Trimestre_2/XML/ProbaImportacionXMLDB/src/main/java/mycompany/probaimportacionxmldb/ProbaImportacionXMLDB.java
@@ -0,0 +1,13 @@
+
+package mycompany.probaimportacionxmldb;
+
+/**
+ *
+ * @author XoanOuteiro
+ */
+public class ProbaImportacionXMLDB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
